c4433193083bee188570c163e3457a4fdf0e349b
Josh White <ciph3rzer0@gmail.com>
2026-02-17 00:15:32 -0500

    Organize dark_world_view

--- autoloads/dark_world_view.gd
+++ autoloads/dark_world_view.gd
@@ -1,5 +1,16 @@
-#@tool
-extends Node
+extends SubViewport
 
-func _ready() -> void:
-	pass
+var source_camera: Camera3D
+
+func set_source_camera(camera: Camera3D) -> void:
+	source_camera = camera
+	if source_camera and has_node("Camera3D"):
+		var viewport_camera = get_node("Camera3D")
+		viewport_camera.fov = source_camera.fov
+		viewport_camera.near = source_camera.near
+		viewport_camera.far = source_camera.far
+
+func _process(_delta: float) -> void:
+	if source_camera and has_node("Camera3D"):
+		var viewport_camera = get_node("Camera3D")
+		viewport_camera.global_transform = source_camera.global_transform
