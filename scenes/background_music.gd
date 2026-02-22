7f85f37370db58e78feabc9b8ee54238c3494425
Josh White <ciph3rzer0@gmail.com>
2026-02-21 19:53:47 -0500

    Get game working in template, fix audio buses to work with volume settings.

--- scenes/background_music.gd
+++ scenes/background_music.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const BLEND_BUS_PREFIX : String = "Blend"
+
 @onready var main_world: AudioStreamPlayer = $BackgroundMusicMainWorld
 @onready var main_world_max_volume_db: float = main_world.volume_db
 
@@ -7,6 +9,8 @@ extends Node
 @onready var dark_world_max_volume_db: float = dark_world.volume_db
 
 const SILENT_DB := -40.0
+
+@export var parent_volume_bus: StringName = &"_BlendMusic"
 
 @export var fade_background_tracks: bool = true
 @export_range(0.0, 1.0, 0.01) var track_fade: float = 0.0:
@@ -18,8 +22,23 @@ const SILENT_DB := -40.0
 
 func _ready() -> void:
 	GameWorld.set_background_music_player(self)
+	_assign_parent_volume_bus()
 	print("Main World:", main_world, " | Dark World:", dark_world)
 	_apply_track_fade()
+
+
+# Re-parent audio stream bus so the dark theme can inherit volume changes from the main theme.
+# (Just has to be this way because of the template)
+func _assign_parent_volume_bus() -> void:
+	if parent_volume_bus == StringName():
+		return
+
+	if AudioServer.get_bus_index(parent_volume_bus) == -1:
+		push_warning("Audio bus '%s' was not found. Keeping existing music buses." % parent_volume_bus)
+		return
+
+	main_world.bus = parent_volume_bus
+	dark_world.bus = parent_volume_bus
 
 
 func _apply_track_fade() -> void:
